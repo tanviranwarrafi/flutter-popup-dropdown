a17d101bb9b7523bac4e144d865092684ef9e8ce
Tanvir Anwar <tanvir.anwar2@gmail.com>
2020-07-07 21:59:19 +0600

    done

---
+++ lib/dropdown.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:popupmenu/model/Company.dart';
+
+class DropDownPage extends StatefulWidget {
+  @override
+  DropDownState createState() => DropDownState();
+}
+
+class DropDownState extends State<DropDownPage> {
+  List<Company> _companies = Company.getCompanies();
+  List<DropdownMenuItem<Company>> _dropdownMenuItems;
+  Company _selectedCompany;
+
+  @override
+  void initState() {
+    _dropdownMenuItems = buildDropdownMenuItems(_companies);
+    _selectedCompany = _dropdownMenuItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<Company>> buildDropdownMenuItems(List companies) {
+    List<DropdownMenuItem<Company>> items = List();
+    for (Company company in companies) {
+      items.add(
+        DropdownMenuItem(
+          value: company,
+          child: Text(
+            company.name,
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+      );
+    }
+    return items;
+  }
+
+  onChangeDropdownItem(Company selectedCompany) {
+    setState(() {
+      _selectedCompany = selectedCompany;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurpleAccent,
+          title: Text("Drop Down List"),
+        ),
+        body: new Container(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Select drop down item: ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    DropdownButton(
+                      value: _selectedCompany,
+                      items: _dropdownMenuItems,
+                      onChanged: onChangeDropdownItem,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Selected item:',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      '${_selectedCompany.name}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.green),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:popupmenu/dropdown.dart';
+import 'package:popupmenu/model/Company.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Company> _companies = Company.getCompanies();
+  List<PopupMenuItem<Company>> _popupMenuItems;
+  Company _selectedCompany;
+
+  @override
+  void initState() {
+    _popupMenuItems = buildPopUPMenuItems(_companies);
+    _selectedCompany = _popupMenuItems[0].value;
+    super.initState();
+  }
+
+  onSelectItem(Company selectedCompany) {
+    setState(() {
+      _selectedCompany = selectedCompany;
+    });
+  }
+
+  List<PopupMenuItem<Company>> buildPopUPMenuItems(List companies) {
+    List<PopupMenuItem<Company>> item = List();
+    for (Company company in companies) {
+      item.add(
+        PopupMenuItem(
+          value: company,
+          child: Text(
+            company.name,
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+      );
+    }
+    return item;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Popup Menu Flutter'),
+        backgroundColor: Colors.deepPurpleAccent,
+        actions: <Widget>[
+          PopupMenuButton(
+            offset: Offset(0, 60),
+            elevation: 5.0,
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  value: 'edit',
+                  child: Text('Edit'),
+                ),
+                PopupMenuItem(
+                  value: 'update',
+                  child: Text('Update'),
+                ),
+                PopupMenuItem(
+                  value: 'delete',
+                  child: Text('Delete'),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'PopUp Menu',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    PopupMenuButton(
+                      offset: Offset(0, 40),
+                      elevation: 5.0,
+                      onSelected: onSelectItem,
+                      itemBuilder: (BuildContext context) {
+                        return buildPopUPMenuItems(_companies);
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Selected Menu:',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      '${_selectedCompany.name}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.green),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40),
+              ],
+            ),
+          ),
+          Align(
+            alignment: FractionalOffset.bottomCenter,
+            child: InkWell(
+              onTap: () {
+                Route route =
+                    MaterialPageRoute(builder: (context) => DropDownPage());
+                Navigator.push(context, route);
+              },
+              child: Container(
+                margin: EdgeInsets.only(left: 16.0, right: 16.0),
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  gradient: LinearGradient(colors: [
+                    Color.fromRGBO(143, 148, 251, 1),
+                    Color.fromRGBO(143, 148, 251, .6),
+                  ]),
+                ),
+                child: Center(
+                  child: Text(
+                    "go to dorop down list page",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 32),
+        ],
+      ),
+    );
+  }
+}
